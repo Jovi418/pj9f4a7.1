783d340f12b685e635375dbc262e709d9c01f2a0
Jovi418 <jvg212@proton.me>
2025-10-28 11:04:51 +0100

    Versió 1.1 del projecte pizzeria

--- codis/pizzeria/app/pizza.php
+++ codis/pizzeria/app/pizza.php
@@ -1,18 +1,18 @@
 <?php
-	$pvp = 8;
-	$IVA=21
+	$pvp = 10;
 	if (empty($_GET['ingr'])) {
-		 echo "El preu de la pizza bàsica és $pvp_public € (IVA inclòs)<br>";
+		echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
+		exit(0);
 	}
 	else {
-		$pvp=((100+$IVA)/100)*(8+count($_GET['ingr']));
+		$IVA=21;
+		$pvp_public=((100+$IVA)/100)*($pvp+count($_GET['ingr']));
 		echo "Has demanat la pizza bàsica amb:<br> ";
 		foreach ($_GET['ingr'] as $ingredient) {
 			if ($ingredient) {
 				echo "* $ingredient<br>";
 			}
 		}
-		echo "El preu és $pvp € (IVA inclòs)<br>";
-		exit(0);
+		echo "El preu és $pvp_public € (IVA inclòs)<br>";
 	}
 ?>
